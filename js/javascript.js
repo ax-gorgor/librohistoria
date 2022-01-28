d6f4467203e124b5d7fa62bb87d7f3b00743fb85
ax-gorgor <80730395+ax-gorgor@users.noreply.github.com>
2022-01-27 22:06:08 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,7 @@ menuBtn.onclick = () =>{
 var swiper = new Swiper(".home-slider", {
     grabCursor: true,
     loop: true,
+    centeredSlides:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
